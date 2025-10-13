4ade839f0c62635dfc5a6fa336f1c1f3fc3dd666
Charliekofg <charliekofg@hotmail.com>
2025-10-13 23:50:48 +0000

    saludos:done_tbd:cards

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -3,13 +3,22 @@ import React from "react";
 //include images into your bundle
 
 import Navbar from "./Navbar";
+import Maincard from "./Maincard";
 
 //create your first component
 const Home = () => {
+	const saludos = [
+		{ encabezado: "A Warm Welcome!", texto: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s" }
+	]
+
 	return (
-		<Navbar />
+		<>
+			<Navbar />
+			<Maincard saludos={saludos} />
+		</>
+
 	);
 
 };
 
-export default Home;
+export default Home;	
---
+++ src/js/components/Maincard.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Maincard = ({saludos}) => {
+    return (
+        <div>
+            <div className="card">
+                {/* <div className="card-header">
+                    Featured
+                </div> */}
+                <div className="card-body">
+                    <h5 className="card-title">{saludos[0].encabezado}</h5>
+                    <p className="card-text">{saludos[0].texto}</p>
+                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+export default Maincard;
