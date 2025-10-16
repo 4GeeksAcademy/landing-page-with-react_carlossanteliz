5d7c80baf20e5a11b271741434b0a615799f0197
Charliekofg <charliekofg@hotmail.com>
2025-10-16 03:53:31 +0000

    Modified rejected project_done

--- src/js/components/Footer.jsx
+++ src/js/components/Footer.jsx
@@ -4,15 +4,8 @@ const Footer = () => {
     return (
 
         <div className="card text-center">
-            {/* <div className="card-header">
-                Featured
-            </div>
-            <div className="card-body">
-                <h5 className="card-title">Special title treatment</h5>
-                <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
-            </div> */}
-            <div className="card-footer bg-dark text-white">
+
+            <div className="card-footer footer-dark bg-dark text-white">
                 Copyright mydreamguitargear.com 2025
             </div>
         </div>
